a1621ce4ebcadba9d511a2c01ac2ffec09911fce
gregoirepuget <gregoirepuget>
2018-10-15 12:32:08 +0200

    end home page

--- wp-content/themes/asside/index.php
+++ wp-content/themes/asside/index.php
@@ -1,6 +1,136 @@
 <?php get_header();
 // get_header("test"); => header-test.php
 ?>
+<?php get_sidebar(); ?>
+
+<main role="main" class="probootstrap-main js-probootstrap-main">
+  <div class="probootstrap-bar">
+    <a href="#" class="probootstrap-toggle js-probootstrap-toggle"><span class="oi oi-menu"></span></a>
+    <div class="probootstrap-main-site-logo"><a href="index.html">Aside</a></a></div>
+  </div>
+  <div class="card-columns">
+    <div class="card">
+      <a href="single.html">
+      <img class="card-img-top probootstrap-animate" src="<?php echo IMAGES_URL."/"; ?>img_1.jpg" alt="Card image cap">
+      </a>
+    </div>
+    <div class="card">
+      <a href="single.html">
+        <img class="card-img-top probootstrap-animate" src="<?php echo IMAGES_URL."/"; ?>img_2.jpg" alt="Card image cap">
+      </a>
+    </div>
+    <div class="card">
+      <a href="single.html">
+        <img class="card-img-top probootstrap-animate" src="<?php echo IMAGES_URL."/"; ?>img_3.jpg" alt="Card image cap">
+      </a>
+    </div>
+    <div class="card">
+      <a href="single.html">
+        <img class="card-img-top probootstrap-animate" src="<?php echo IMAGES_URL."/"; ?>img_4.jpg" alt="Card image cap">
+      </a>
+    </div>
+    <div class="card">
+      <a href="single.html">
+        <img class="card-img-top probootstrap-animate" src="<?php echo IMAGES_URL."/"; ?>img_5.jpg" alt="Card image cap">
+      </a>
+    </div>
+    <div class="card">
+      <a href="single.html">
+        <img class="card-img-top probootstrap-animate" src="<?php echo IMAGES_URL."/"; ?>img_6.jpg" alt="Card image cap">
+      </a>
+    </div>
+    <div class="card">
+      <a href="single.html">
+        <img class="card-img-top probootstrap-animate" src="<?php echo IMAGES_URL."/"; ?>img_7.jpg" alt="Card image cap">
+      </a>
+    </div>
+    <div class="card">
+      <a href="single.html">
+        <img class="card-img-top probootstrap-animate" src="<?php echo IMAGES_URL."/"; ?>img_8.jpg" alt="Card image cap">
+      </a>
+    </div>
+    <div class="card">
+      <a href="single.html">
+        <img class="card-img-top probootstrap-animate" src="<?php echo IMAGES_URL."/"; ?>img_9.jpg" alt="Card image cap">
+      </a>
+    </div>
+    <div class="card">
+      <a href="single.html">
+        <img class="card-img-top probootstrap-animate" src="<?php echo IMAGES_URL."/"; ?>img_10.jpg" alt="Card image cap">
+      </a>
+    </div>
+    <div class="card">
+      <a href="single.html">
+        <img class="card-img-top probootstrap-animate" src="<?php echo IMAGES_URL."/"; ?>img_11.jpg" alt="Card image cap">
+      </a>
+    </div>
+    <div class="card">
+      <a href="single.html">
+        <img class="card-img-top probootstrap-animate" src="<?php echo IMAGES_URL."/"; ?>img_12.jpg" alt="Card image cap">
+      </a>
+    </div>
+    <div class="card">
+      <a href="single.html">
+        <img class="card-img-top probootstrap-animate" src="<?php echo IMAGES_URL."/"; ?>img_13.jpg" alt="Card image cap">
+      </a>
+    </div>
+    <div class="card">
+      <a href="single.html">
+        <img class="card-img-top probootstrap-animate" src="<?php echo IMAGES_URL."/"; ?>img_14.jpg" alt="Card image cap">
+      </a>
+    </div>
+    <div class="card">
+      <a href="single.html">
+        <img class="card-img-top probootstrap-animate" src="<?php echo IMAGES_URL."/"; ?>img_15.jpg" alt="Card image cap">
+      </a>
+    </div>
+    <div class="card">
+      <a href="single.html">
+        <img class="card-img-top probootstrap-animate" src="<?php echo IMAGES_URL."/"; ?>img_16.jpg" alt="Card image cap">
+      </a>
+    </div>
+    <div class="card">
+      <a href="single.html">
+        <img class="card-img-top probootstrap-animate" src="<?php echo IMAGES_URL."/"; ?>img_17.jpg" alt="Card image cap">
+      </a>
+    </div>
+    <div class="card">
+      <a href="single.html">
+        <img class="card-img-top probootstrap-animate" src="<?php echo IMAGES_URL."/"; ?>img_18.jpg" alt="Card image cap">
+      </a>
+    </div>
+    <div class="card">
+      <a href="single.html">
+        <img class="card-img-top probootstrap-animate" src="<?php echo IMAGES_URL."/"; ?>img_19.jpg" alt="Card image cap">
+      </a>
+    </div>
+    <div class="card">
+      <a href="single.html">
+        <img class="card-img-top probootstrap-animate" src="<?php echo IMAGES_URL."/"; ?>img_20.jpg" alt="Card image cap">
+      </a>
+    </div>
+    <div class="card">
+      <a href="single.html">
+        <img class="card-img-top probootstrap-animate" src="<?php echo IMAGES_URL."/"; ?>img_21.jpg" alt="Card image cap">
+      </a>
+    </div>
+  </div>
+
+  <div class="container-fluid d-md-none">
+    <div class="row">
+      <div class="col-md-12">
+        <ul class="list-unstyled d-flex probootstrap-aside-social">
+          <li><a href="#" class="p-2"><span class="icon-twitter"></span></a></li>
+          <li><a href="#" class="p-2"><span class="icon-instagram"></span></a></li>
+          <li><a href="#" class="p-2"><span class="icon-dribbble"></span></a></li>
+        </ul>
+        <p>&copy; 2017 <a href="https://uicookies.com/" target="_blank">uiCookies:Aside</a>. <br> All Rights Reserved. Designed by <a href="https://uicookies.com/" target="_blank">uicookies.com</a></p>
+      </div>
+    </div>
+  </div>
+
+</main>
+
 
 
 <?php get_footer(); ?>
