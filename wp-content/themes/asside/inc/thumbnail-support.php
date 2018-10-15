0ba3cf24b0e97475a7e9192fcd7f687419b82592
gregoirepuget <gregoirepuget>
2018-10-15 12:58:39 +0200

    feature thumbnail

---
+++ wp-content/themes/asside/inc/thumbnail-support.php
@@ -0,0 +1,8 @@
+<?php
+add_action( 'after_setup_theme', 'thumbnails_theme_support' );
+function thumbnails_theme_support(){
+    add_theme_support( 'post-thumbnails' );
+
+    add_image_size( 'page-thumb', 1700, 1133, true );
+    add_image_size( 'portfolio-thumb', 600, 99999, false );
+}
