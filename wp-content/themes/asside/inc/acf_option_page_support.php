ba5c50e5b2533e54abaa01bec134eb6728d1f154
gregoirepuget <gregoirepuget>
2018-10-15 17:38:07 +0200

    hotfix acf option page

--- wp-content/themes/asside/inc/acf_option_page_support.php
+++ wp-content/themes/asside/inc/acf_option_page_support.php
@@ -1,7 +1,10 @@
 <?php
-add_action("init", 'add_acf_option_page_support')
+add_action("init", 'add_acf_option_page_support');
+
+function add_acf_option_page_support()
 {
-  if( function_exists('acf_add_options_page') ) {
+  if( function_exists('acf_add_options_page') )
+  {
 	   acf_add_options_page();
   }
 
